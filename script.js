326d851acdb36ad5400984146b3efed6b0fd6558
Gene Caleb Carbonilla <carbonillacaleb@gmail.com>
2022-06-08 13:20:58 +0800

    Chatbot Website

---
+++ script.js
@@ -0,0 +1,7 @@
+let mainNav = document.getElementById("nav");
+let navBarToggle = document.getElementById("hb-btn");
+
+navBarToggle.addEventListener("click", fucntion() {
+
+    mainNav.classList.toggle("active");
+});
